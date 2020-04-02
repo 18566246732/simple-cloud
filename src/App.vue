6a67f9728b8616a3e664fa4fad6baf8e4c0dd2a4
ivanliu <ivanliu@wezhuiyi.com>
2020-04-02 16:29:08 +0800

    feat: 字体切换成PingFang SC Heavy

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,22 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: "PingFang SC M";
+  src: url('./assets/font/PingFangSC.ttf');
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: "PingFang SC M", "Source Sans Pro", "Helvetica Neue", "Microsoft Yahei", Helvetica, Arial, "Hiragino Sans GB", "Heiti SC", "WenQuanYi Micro Hei", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+* {
+  box-sizing: border-box;
 }
 </style>
