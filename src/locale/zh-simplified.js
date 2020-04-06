178c7208b8474f5c18c89728c772199e8b57b538
ivanliu <ivanliu@wezhuiyi.com>
2020-04-07 07:45:40 +0800

    feat: 完成头部繁简体数据

--- src/locale/zh-simplified.js
+++ src/locale/zh-simplified.js
@@ -1,17 +1,19 @@
+// 页面所有文本的切换思路一样，暂时只展示导航栏部分
+
 export const zhSimplified = {
   header: {
-      menu: {
-          homepage: "首页",
-          solution: "解决方案",
-          cases: "客户案例",
-          function: "功能",
-          price: "定价",
-          customize: "定制",
-          help: "帮助"
-      },
-      action: {
-          register: "注册",
-          login: "登录"
-      }
+    menu: {
+        homepage: "首页",
+        solution: "解决方案",
+        cases: "客户案例",
+        function: "功能",
+        price: "定价",
+        customize: "定制",
+        help: "帮助"
+    },
+    action: {
+        register: "注册",
+        login: "登录"
+    }
   }
 };
--- src/locale/zh-traditional.js
+++ src/locale/zh-traditional.js
@@ -1,10 +1,19 @@
+// 页面所有文本的切换思路一样，暂时只展示导航栏部分
+
 export const zhTraditional = {
-    header: {
-        menu: {
-            homepage: "首頁"
-        },
-        action: {
-            register: ""
-        }
+  header: {
+    menu: {
+      homepage: "首頁",
+      solution: "解決方案",
+      cases: "客戶案例",
+      function: "功能",
+      price: "定價",
+      customize: "定制",
+      help: "幫助"
+    },
+    action: {
+        register: "註冊",
+        login: "登錄"
     }
+  }
 };
