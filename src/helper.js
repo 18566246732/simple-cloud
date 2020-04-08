17ba69971fb67c8dd373152fdbce67a35a4afe89
ivanliu <ivanliu@wezhuiyi.com>
2020-04-08 18:53:10 +0800

    feat: add .vscode

--- src/helper.js
+++ src/helper.js
@@ -5,9 +5,9 @@ export const helper = {
   /**
    * 根据设定的时间间隔，自动执行某个动作
    *
-   * @param {number} index 需要的动作的下标
-   * @param {object} options 激活方式，可以设置立即执行
-   * @param {number} interval 执行的间隔
+   * @param {Number} index 需要的动作的下标
+   * @param {Object} options 激活方式，可以设置立即执行
+   * @param {Number} interval 执行的间隔
    */
   rotateActions(index = 0, fn, options = {}, interval = 3000) {
     // 先清除，防止定时器堆积
